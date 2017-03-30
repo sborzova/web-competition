4608dadd5c902a66d462e54c1db32b163a77eae7
Silvia Borzová <borzovasilvia@gmail.com>
2017-03-30 17:01:31 +0200

    deleted cookie-parser, serve-favicon dependencies

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
-var multer = require("multer");
 
 var appRoutes = require('./routes/app');
 var userRoutes = require('./routes/user');
@@ -72,7 +69,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
