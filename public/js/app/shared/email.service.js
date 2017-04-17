630bf64bf6d42bb5c424dbbf23191380606f2268
Silvia Borzová <borzovasilvia@gmail.com>
2017-04-17 14:35:26 +0200

    email route delete method

--- public/js/app/shared/email.service.js
+++ public/js/app/shared/email.service.js
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http, Headers } from "@angular/http";
+import { Http } from "@angular/http";
 import { FlashMessageService } from "../flash-message/flash-messages.service";
 import { Observable } from "rxjs";
 export var EmailService = (function () {
@@ -9,17 +9,6 @@ export var EmailService = (function () {
         var routeModule = require("../app.routing");
         this.hostUrl = routeModule.hostUrl;
     }
-    EmailService.prototype.sendEmail = function (email) {
-        var body = JSON.stringify(email);
-        var headers = new Headers({ 'Content-Type': 'application/json' });
-        return this.http.post(this.hostUrl.concat('email'), body, { headers: headers })
-            .map(function (response) {
-            return response.json();
-        })
-            .catch(function (error) {
-            return Observable.throw(error);
-        });
-    };
     EmailService.prototype.sendEmailNewPassword = function (email) {
         return this.http.post(this.hostUrl.concat('emailpassword/') + email, '')
             .map(function (response) {
--- routes/email.js
+++ routes/email.js
@@ -3,35 +3,6 @@ var router = express.Router();
 
 var User = require('../models/user');
 
-router.post('/email', function (req, res) {
-    var helper = require('sendgrid').mail;
-    var from_email = new helper.Email('no-reply@test-cttcompetition.com');
-    var to_email = new helper.Email(req.body.receiver);
-    var subject = req.body.subject;
-    var content = new helper.Content('text/plain', req.body.content);
-    var mail = new helper.Mail(from_email, subject, to_email, content);
-
-    var sg = require('sendgrid')('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');
-    var request = sg.emptyRequest({
-        method: 'POST',
-        path: '/v3/mail/send',
-        body: mail.toJSON(),
-    });
-
-    // sg.API(request, function(error, response) {
-    //     if (error){
-    //         return res.status(500).json({
-    //             title: 'An error occurred',
-    //             error: err
-    //         });
-    //     }
-    //
-    //     return res.status(200).json({
-    //         message: 'Email send'
-    //     });
-    // });
-});
-
 /**
  *  change password when forgotten
  */
@@ -68,21 +39,21 @@ router.post('/emailpassword/:email', function (req, res, next) {
             + 'If you did not request this, please ignore this email.');
         var mail = new helper.Mail(from_email, subject, to_email, content);
 
-        var sg = require('sendgrid')('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');
+        var sg = require('sendgrid')();
         var request = sg.emptyRequest({
             method: 'POST',
             path: '/v3/mail/send',
             body: mail.toJSON(),
         });
 
-        // sg.API(request, function(error, response) {
-        //     if (error){
-        //         return res.status(500).json({
-        //             title: 'An error occurred',
-        //             error: err
-        //         });
-        //     }
-        // });
+        sg.API(request, function(error, response) {
+            if (error){
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+        });
 
         user.save(function (err, result) {
             if (err) {
